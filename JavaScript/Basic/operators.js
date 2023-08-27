61feede8f07f704fc68349fae10f469707787136
Shougotmollik <shougotmollik@gmail.com>
2023-08-27 13:24:40 +0600

    adding operator

---
+++ JavaScript/Basic/operators.js
@@ -0,0 +1,7 @@
+food=20
+tipparcentace=0.2
+tipAmount=food*tipparcentace
+console.log("The Food price is "+food);
+console.log("Tip amount is : "+tipAmount);
+totalPay=food+tipAmount
+console.log("The Total amount to pay " +totalPay)
