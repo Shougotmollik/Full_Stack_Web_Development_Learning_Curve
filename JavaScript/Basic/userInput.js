9b5ded5e9f66741ce1aba05770f4a79c5ff1c503
Shougotmollik <shougotmollik@gmail.com>
2023-08-27 14:36:21 +0600

    adding user input project

--- JavaScript/Basic/userInput.js
+++ JavaScript/Basic/userInput.js
@@ -1,4 +1,10 @@
 const prompt = require('prompt-sync')();
 
-let name=prompt("Whats your name ?")
-console.log(name);
+foodbill=Number(prompt("Enter your Bill : "))
+tipPercnetace=Number(prompt("Enter your tips percentace "))/100
+tipAmount=foodbill*tipPercnetace
+TotalAmount=foodbill+tipAmount
+
+console.log("Your Food bill is "+foodbill)
+console.log("Your tips ammount " +tipAmount)
+console.log("Your total bill amount is "+TotalAmount)
