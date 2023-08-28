d00ba0a95bd8a5c36716a46ce2684b5ccbc9ef5e
Shougotmollik <shougotmollik@gmail.com>
2023-08-28 20:28:57 +0600

    adding object

--- JavaScript/Basic/object.js
+++ JavaScript/Basic/object.js
@@ -6,7 +6,20 @@ const person={
 }
 
 // log out the using . 
-console.log(person.first_name);
+//console.log(person.first_name);
 
 //log our with braket []
-console.log(person['last_name']);
+//console.log(person['last_name']);
+
+
+//creating a arrow function along with object 
+
+const introduction=(name,age)=>{
+    const person={
+        name:name,
+        age:age
+    }
+    const intro =` Hi my name is ${name}.I am ${age} years old.`
+    return intro
+}
+console.log(introduction("shougot mollik","20"));
