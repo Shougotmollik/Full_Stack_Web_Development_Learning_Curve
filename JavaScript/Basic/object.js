9aef8e604cc168ffff7ca0c1b552eda551e7697f
Shougotmollik <shougotmollik@gmail.com>
2023-08-28 20:20:33 +0600

    adding object

---
+++ JavaScript/Basic/object.js
@@ -0,0 +1,12 @@
+//Creating a object 
+
+const person={
+    first_name:'shougot',
+    last_name:'mollik'
+}
+
+// log out the using . 
+console.log(person.first_name);
+
+//log our with braket []
+console.log(person['last_name']);
