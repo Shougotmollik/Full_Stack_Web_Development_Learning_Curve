e300648d9ee34b6ea05645b6afbcb2b2b58f7105
Shougotmollik <shougotmollik@gmail.com>
2023-08-28 21:57:22 +0600

    adding loops

---
+++ JavaScript/Basic/loops.js
@@ -0,0 +1,19 @@
+//count 1 to 10
+for(let i=1;i<=10;i++){
+   // console.log(i);
+}
+
+const numbers=[1,2,3,4,5,6,7,8,9]
+for(const number of numbers){
+   // console.log(number);
+   // console.log(number*2);
+}
+
+//Get double value array from given value 
+
+const numericals=[1,2,3,4,5,6,7,8,9]
+let result=[]
+for(const numerical of numericals){
+    result.push(numerical*2)
+}
+console.log(result);
