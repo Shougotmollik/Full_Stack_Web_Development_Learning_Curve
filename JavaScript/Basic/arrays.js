4371c70f3588150d2fdda7cf0d06040539382d6c
Shougotmollik <shougotmollik@gmail.com>
2023-08-28 13:33:30 +0600

    adding arrays

--- JavaScript/Basic/arrays.js
+++ JavaScript/Basic/arrays.js
@@ -1,3 +1,10 @@
 const fruits=["banana","apple","pineapple","pear"]
 console.log(fruits);
-console.log(fruits[3]);
+//console.log(fruits[3]);
+
+fruits.push("orange")
+console.log(fruits);
+
+//length count of arrays 
+
+console.log(fruits.length);
