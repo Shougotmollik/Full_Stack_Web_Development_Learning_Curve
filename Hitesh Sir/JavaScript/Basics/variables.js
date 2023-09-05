7782779e22211770a02907de8bf22ea9d511659e
Shougotmollik <shougotmollik@gmail.com>
2023-09-05 14:52:21 +0600

    adding variables

---
+++ Hitesh Sir/JavaScript/Basics/variables.js
@@ -0,0 +1,22 @@
+// initilize all variables 
+
+const accountId=1464863  //value always fixed
+let accountEmail="shougot@web.dev"
+var accountPassword="password"
+accountCity="kolkata"
+let accountState
+
+
+//change all values 
+accountEmail="mollik@web.dev"
+accountPassword="paswworfjsh"
+accountCity="mumbai"
+
+/*
+not use var => because proble in scop and function
+ */
+
+// display all and test one by one
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
