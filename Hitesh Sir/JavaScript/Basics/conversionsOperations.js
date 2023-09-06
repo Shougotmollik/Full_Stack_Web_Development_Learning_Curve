62da36212d5e6310ade0de0fd9a39bde15bf979b
Shougotmollik <shougotmollik@gmail.com>
2023-09-06 21:46:53 +0600

    conversions data types

--- Hitesh Sir/JavaScript/Basics/conversionsOperations.js
+++ Hitesh Sir/JavaScript/Basics/conversionsOperations.js
@@ -1,12 +1,12 @@
 let score="23ieo"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 //convertion 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 /*
 Data type conversion 
@@ -17,3 +17,40 @@ Data type conversion
 5."" => false 
 6."shougot"=> true
 */
+
+/*********************** Operations *************************/
+
+let number=20
+let negNumber=-number
+// console.log(negNumber);
+
+// All Arithmetic operations needs to know 
+
+// Add two String 
+
+str1="Hey "
+str2="Shougot"
+addingString=str1+str2
+console.log(addingString);
+
+// Postfix increment
+
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+console.log(y);
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+console.log(y2);
+
+
+// Prefix increment
+let a = 3;
+const b = ++x;
+// x is 4; y is 4
+
+let a2 = 3n;
+const b2 = ++a2;
+// x2 is 4n; y2 is 4n
