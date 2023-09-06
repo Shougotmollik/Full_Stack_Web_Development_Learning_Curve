5a6bae9edc2aab70349b3680c16f4c3e52bd957b
Shougotmollik <shougotmollik@gmail.com>
2023-09-06 12:49:44 +0600

    adding Data type

--- Hitesh Sir/JavaScript/Basics/dataTypes.js
+++ Hitesh Sir/JavaScript/Basics/dataTypes.js
@@ -1,3 +1,28 @@
 "use strict"; //treat all JS code as newer version 
 
 //alert(2+4)
+
+// String 
+let name="Shougot mollik"
+
+// number => Range 2 to power 53
+let age="21"
+let tempature=32.5
+
+//boolean 
+let IsAdult=true
+
+//null => stand alone value /empty value  
+let glass=null
+//undefined => no value give 
+let water;
+//symbol => for unique ness
+
+//display all 
+
+console.table([name,age,tempature,IsAdult,glass,water])
+
+//object
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object 
