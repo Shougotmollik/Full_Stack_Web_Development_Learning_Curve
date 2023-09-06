f98778da9f14871c2f19a38dfecb18bcd24a7e4a
Shougotmollik <shougotmollik@gmail.com>
2023-09-06 22:24:18 +0600

    Data type Summery

---
+++ Hitesh Sir/JavaScript/Basics/dataType_summery.js
@@ -0,0 +1,36 @@
+/*
+Data Type 2 types.
+1.perimtive 
+   -String
+   -Number
+   -Boolean
+   -Null
+   -Undefined 
+   -symbol
+   -BigInt
+2.Non-perimative
+   -Array
+   -Objects
+   -Function
+*/
+
+//perimative example
+let name="Shougot"
+let age =21
+let isAdult=true
+let water=null;
+let seaLevel;
+let drink=Symbol(239)
+let numbers=BigInt (37562438724n)
+
+//Non perimative example 
+
+let animals=["Cat","Cow","Tiger","Monkey","Dog"]
+let introduction={
+   name:"Shougot",
+   age:21,
+   Degree:"CSE"
+}
+let helloWorld=function(){
+   console.log("Hello World");
+}
