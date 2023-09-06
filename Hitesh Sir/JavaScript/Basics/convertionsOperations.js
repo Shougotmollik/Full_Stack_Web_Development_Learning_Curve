a2fdc1dec5467eff7ce4d87bcf425ef5ccc57e1b
Shougotmollik <shougotmollik@gmail.com>
2023-09-06 13:46:30 +0600

    data type conversin

---
+++ Hitesh Sir/JavaScript/Basics/convertionsOperations.js
@@ -0,0 +1,19 @@
+let score="23ieo"
+
+console.log(typeof score);
+
+//convertion 
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+/*
+Data type conversion 
+
+1.number to number
+2."string""33ac"=NaN 
+4.true=1,false=0
+5."" => false 
+6."shougot"=> true
+*/
