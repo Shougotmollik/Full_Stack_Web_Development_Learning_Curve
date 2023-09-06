2768b169af96514fe635c02cfe15d8d0b908b0f3
Shougotmollik <shougotmollik@gmail.com>
2023-09-06 21:58:42 +0600

    compariosns

---
+++ Hitesh Sir/JavaScript/Basics/comparisons.js
@@ -0,0 +1,8 @@
+// console.log("2">1);
+// console.log("02">1);
+
+// Null comparisons 
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0); //true => comparison convert null into 0 for this reason its true.
