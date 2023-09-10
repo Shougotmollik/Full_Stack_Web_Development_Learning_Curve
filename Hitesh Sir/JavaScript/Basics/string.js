7243dac4ae0a2664343be4179f28e227ffa17379
Shougotmollik <shougotmollik@gmail.com>
2023-09-10 14:56:25 +0600

    strings

--- Hitesh Sir/JavaScript/Basics/string.js
+++ Hitesh Sir/JavaScript/Basics/string.js
@@ -1,3 +1,4 @@
 const name="Shougot Mollik  "
 const repoCount=50
-console.log(name + repoCount+"value");
+// console.log(name + repoCount+"value");
+console.log(`Hi my name is ${name}and my repo count is ${repoCount}`);
