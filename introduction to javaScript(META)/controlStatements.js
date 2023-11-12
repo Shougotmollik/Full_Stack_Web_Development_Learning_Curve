885922e80cb378695bd3f859c8857b576502fc99
Shougot mollik <shougotmollik@gmail.com>
2023-11-13 02:36:32 +0600

    control statements

---
+++ introduction to javaScript(META)/controlStatements.js
@@ -0,0 +1,17 @@
+var result=50;
+
+if (result>33) {
+ console.log("Your are pass the examination");
+} else {
+ console.log("Sorry you are fail");
+}
+
+
+for(var i=1;i<=3;i++){
+ console.log(i);
+}
+console.log("Go!");
+
+for (var i = 0; i <= 3; i++) {
+ console.log("This is line ", i)
+}
