d5245b2bd5c5025d6e733b88db963e63101af096
Shougot mollik <shougotmollik@gmail.com>
2023-11-14 00:56:16 +0600

    error handling

---
+++ introduction to javaScript(META)/errorHandling.js
@@ -0,0 +1,93 @@
+// var food;
+// console.log(food);
+
+
+// var result = null;
+// console.log(result);
+
+
+// try {
+//  console.log('Hello');
+// } catch(err) {
+//  console.log('Goodbye');
+// }
+
+
+// var x;
+
+// if(x === null) {
+//   console.log("null");
+// } else if(x === undefined) {
+//   console.log("undefined");
+// } else {
+//   console.log("ok");
+// }
+
+
+
+// throw new Error();
+// console.log("Hello");
+
+
+
+// var x = {};
+// console.log(typeof(x));
+
+
+// try {
+//  console.log('hello)
+//  } catch(e) {
+//  console.log('caught')
+//  }
+
+
+// var burger = ["bun", "beef", "lettuce", "tomato sauce", "onion", "bun"];
+// console.log(burger[2]);
+
+
+
+// var bicycle = {
+//  wheels: 2,
+//  start: function() {
+
+//  },
+//  stop: function() {
+
+//  }
+// };
+
+// console.log(bicycle)
+
+
+
+// try {
+//  throw new Error();
+//  console.log('Hello');
+// } catch(err) {
+//  console.log('Goodbye');
+// }
+
+
+
+// function add(a, b) {
+//  console.log(a + b)
+// }
+
+// add(3, "4");
+
+
+
+// var result;
+// console.log(result);
+
+// var str = "Hello";
+// str.match("jello");
+
+// console.log(str);
+
+
+try {
+ Number(5).toPrecision(300)
+ } catch(e) {
+ console.log("There was an error")
+ }
