c696df7c99da097b0b0356186eb6347a830cdf91
Shougot mollik <shougotmollik@gmail.com>
2023-11-14 00:35:40 +0600

    function

---
+++ introduction to javaScript(META)/function.js
@@ -0,0 +1,24 @@
+// function addTwoNumbers(){
+//  var a=10;
+//  var b=20;
+//  var sum=a+b;
+//  console.log(sum);
+// }
+
+// addTwoNumbers();
+
+// function addTwoNumber(a,b){
+//  var sum =a+b;
+//  console.log(sum);
+// }
+
+// addTwoNumber(5,7)
+
+
+// var number=Math.ceil(2.49)
+// console.log(number);
+
+var parting ="Goodbye";
+var name="robin";
+
+console.log(parting.concat(name) );
