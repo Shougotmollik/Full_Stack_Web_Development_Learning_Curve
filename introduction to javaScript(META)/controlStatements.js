f58bd3f1d2f339e9fdd9eda5a1a04f0aa0cc0695
Shougot mollik <shougotmollik@gmail.com>
2023-11-14 00:35:25 +0600

    control statement

--- introduction to javaScript(META)/controlStatements.js
+++ introduction to javaScript(META)/controlStatements.js
@@ -1,17 +1,165 @@
-var result=50;
+// var result=50;
 
-if (result>33) {
- console.log("Your are pass the examination");
+// if (result>33) {
+//  console.log("Your are pass the examination");
+// } else {
+//  console.log("Sorry you are fail");
+// }
+
+
+// for(var i=1;i<=3;i++){
+//  console.log(i);
+// }
+// console.log("Go!");
+
+// for (var i = 0; i <= 3; i++) {
+//  console.log("This is line ", i)
+// }
+
+
+// var i = 3;
+// var j = 5;
+
+// if(i == 3 && j < 5) {
+//   console.log("Hello");
+// } else {
+//   console.log("Goodbye");
+// }
+
+
+// var i = 7;
+// var j = 2;
+
+// if(i < 7 || j < 5) {
+//   console.log("Hello");
+// } else {
+//   console.log("Goodbye");
+// }
+
+
+// if(i < 5) {
+//  console.log("Hello");
+// } else {
+//  console.log("Goodbye");
+// }
+
+
+// if(i == 0 && i == 1) {
+//  console.log("Hello");
+// } else {
+//  console.log("Goodbye");
+// }
+
+
+
+// // for (i = 0; i < 2; i++) {
+// //  console.log("Hello");
+// // }
+
+
+
+// //  var i = 0;
+// // while(i < 3) {
+// //   console.log("Hello");
+// //   i++;
+// // }
+
+
+
+// // for (i = 0; i < 2; i++) {
+// //  for (var j = 0; j < 3; j++) {
+// //      console.log("Hello");
+// //  }
+// // }
+
+
+
+// // if(i <= 5) {
+// //  console.log("Hello");
+// // } else if(i <= 10) {
+// //  console.log("Goodnight");
+// // } else {
+// //  console.log("Goodbye");
+// // }
+
+
+
+// switch(i) {
+//  case 1:
+//    console.log("Hello");
+//    break;
+//  case 2:
+//    console.log("Goodnight");
+//    break;
+//  case 3:
+//    console.log("Goodbye");
+//    break;
+// }
+
+
+// if(i == 2 || i == 3) {
+//  console.log("Hello");
+// } else {
+//  console.log("Goodbye");
+// }
+
+
+
+// for (i = 0; i < 2; i++) {
+//  console.log("Hello");
+// }
+
+
+
+// var i = 0;
+// while(i < 3) {
+//   console.log("Hello");
+//   i++;
+// }
+
+
+
+// for (i = 0; i < 2; i++) {
+//  for (var j = 0; j < 3; j++) {
+//      console.log("Hello");
+//  }
+// }
+
+// var i=7
+
+// if(i <= 5) {
+//  console.log("Hello");
+// } else if(i <= 10) {
+//  console.log("Goodnight");
+// } else {
+//  console.log("Goodbye");
+// }
+
+
+// var i=3
+// switch(i) {
+//  case 1:
+//    console.log("Hello");
+//    break;
+//  case 2:
+//    console.log("Goodnight");
+//    break;
+//  case 3:
+//    console.log("Goodbye");
+//    break;
+// }
+
+
+// var i=3
+// if(i == 2 || i == 3) {
+//  console.log("Hello");
+// } else {
+//  console.log("Goodbye");
+// }
+
+var i=1
+if(i == 0 && i == 1) {
+ console.log("Hello");
 } else {
- console.log("Sorry you are fail");
-}
-
-
-for(var i=1;i<=3;i++){
- console.log(i);
-}
-console.log("Go!");
-
-for (var i = 0; i <= 3; i++) {
- console.log("This is line ", i)
+ console.log("Goodbye");
 }
