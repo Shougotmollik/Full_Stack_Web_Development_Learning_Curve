26448837cec6b977fb5429648d9a01b0b7d06ac5
Shougot mollik <shougotmollik@gmail.com>
2023-11-14 00:35:32 +0600

    data type

--- introduction to javaScript(META)/dataTyepe.js
+++ introduction to javaScript(META)/dataTyepe.js
@@ -1,6 +1,11 @@
-console.log("hello"+10);
+// console.log("hello"+10);
 
-var i = 7;
-i += 1;
-i += 2;
-console.log(i);
+// var i = 7;
+// i += 1;
+// i += 2;
+// console.log(i);
+
+
+for(var i = 0; i <= 5; i++) {
+ console.log("Hello");
+}
