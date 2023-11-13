87f1d6f8f3b9bc8449fd115c023112588ea23dac
Shougot mollik <shougotmollik@gmail.com>
2023-11-14 00:35:49 +0600

    objects

---
+++ introduction to javaScript(META)/object.js
@@ -0,0 +1,30 @@
+// var result = "Hello".indexOf('l');
+// console.log(result);
+
+
+// var clothes = [];
+// clothes.push('gray t-shirt');
+// clothes.push('green scarf');
+// clothes.pop();
+// clothes.push('slippers');
+// clothes.pop();
+// clothes.push('boots');
+// clothes.push('old jeans');
+// console.log(clothes);
+
+// var food = [];
+// food.push('Chocolate');
+// food.push('Ice cream');
+// food.push('Donut');
+
+// console.log(food[1])
+
+
+var dog = {
+ color: "brown",
+ height: 30,
+ length: 60,
+};
+dog["type"] = "corgi";
+
+console.log(dog.length);
