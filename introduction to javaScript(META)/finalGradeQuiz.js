dd7d59ed19d060d6716057ed02e6e0f826c22b7c
Shougot mollik <shougotmollik@gmail.com>
2023-11-17 01:01:18 +0600

    final grade Quiz

---
+++ introduction to javaScript(META)/finalGradeQuiz.js
@@ -0,0 +1,106 @@
+
+// const a = true;
+// if(!a) {
+//     console.log("Green");
+// } else {
+//     console.log("Blue");
+// }
+
+
+// var x = true;
+// x = 23;
+// console.log(x);
+
+
+
+// var x = 0 != 1;
+// console.log(x);
+
+
+// var x = 10;
+
+// if(x > 10) {
+//     console.log("Apple");
+// } else if(x > 5) {
+//     console.log("Pear");
+// } else {
+//     console.log("Orange");
+// }
+
+
+
+// var result = 0;
+
+// for(var i = 0; i < 5; i++) {
+//     result += 2;
+// }
+
+// console.log(result);
+
+
+
+// var result;
+// console.log(result);
+// result = 7;
+
+
+
+// var car = { mileage: 200 }
+// var carMileage = 100
+// console.log(car.mileage)
+
+
+
+// var veggies = []
+// veggies.push('parsley')
+// veggies.push('carrot')
+// console.log(veggies[2])
+
+
+// const addFive = require('./addFive')
+
+
+
+// var x = 2;
+// x += 5;
+// console.log(x);
+
+
+
+// var result = 0;
+
+// var i = 0;
+// var limit = 3;
+// while(i < limit) {
+//     result += 2;
+//     i++;
+// }
+
+// console.log(result);
+
+
+
+// console.log(result);
+// var result = 7;
+
+
+// function addTwo(a,b) {
+//  return a
+// }
+// addTwo(5,10)
+
+
+
+// var veggies = ['parsley', 'carrot']
+// console.log(veggies[2])
+
+
+
+
+
+
+
+var cat = {}
+cat["sound"] = "meow"
+var catSound = "purr"
+console.log(cat.sound)
