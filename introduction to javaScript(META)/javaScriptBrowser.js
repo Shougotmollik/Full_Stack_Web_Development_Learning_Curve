98b1e81f3f0606e820dc4629d5d6eaa88756fd8e
Shougot mollik <shougotmollik@gmail.com>
2023-11-16 22:04:08 +0600

    javscript in browser

---
+++ introduction to javaScript(META)/javaScriptBrowser.js
@@ -0,0 +1,89 @@
+
+{/* <script type="text/javascript">
+//Comment
+</script> */}
+
+// getElementById('main-heading')
+
+
+// var result = {
+//  value: 7
+// };
+// console.log(JSON.stringify(result));
+
+
+
+// function scopeTest() {
+//  var y = 44;
+
+//  console.log(x);
+// }
+
+// var x = 33;
+// scopeTest();
+
+
+// class Cake {
+//  constructor(lyr) {
+//      this.layers = lyr;
+//  }
+
+//  getLayers() {
+//      return this.layers;
+//  }
+// }
+
+// class WeddingCake extends Cake {
+//  constructor() {
+//      super(2);
+//  }
+
+//  getLayers() {
+//      return super.getLayers() * 5;
+//  }
+// }
+
+// var result = new WeddingCake();
+// console.log(result.getLayers());
+
+
+
+// class Animal {
+
+// }
+
+// class Dog extends Animal {
+//     constructor() {
+//         super();
+//         this.noise = "bark";
+//     }
+
+//     makeNoise() {
+//       return this.noise;
+//     }
+// }
+
+// class Wolf extends Dog {
+//     constructor() {
+//         super();
+//         this.noise = "growl";
+//     }
+// }
+
+// var result = new Wolf();
+// console.log(result.makeNoise());
+
+
+// const [a,b]=[1,2,3,4]
+// console.log(b);
+
+
+// function count(...food) {
+//  console.log(food.length)
+// }
+
+// count("Burgers", "Fries", null);
+
+
+const letter = "a"
+letter = "b"
