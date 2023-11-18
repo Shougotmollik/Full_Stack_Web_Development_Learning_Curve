d33203fba6f58e3b4268a0b2864ecfdb0e02dd87
Shougot mollik <shougotmollik@gmail.com>
2023-11-18 20:51:21 +0600

    array class

--- Data Structure And Backend Development With Java/array.java
+++ Data Structure And Backend Development With Java/array.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
+
 public class array {
     public static void main(String[] args) {
-        int arr[][]={
+        int [][]arr={
                 {22,33,44,566,66},
                 {44,66,777,777}
         };
         int []hello={5,6,7,8,6};
-        System.out.println(arr);
+        System.out.println(Arrays.stream(arr).toArray());
+        System.out.println(hello);
     }
 }
