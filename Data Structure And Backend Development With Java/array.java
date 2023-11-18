3b5a642804324102b9c8613d0b3073fa794bd77c
Shougot mollik <shougotmollik@gmail.com>
2023-11-18 20:48:56 +0600

    array class

---
+++ Data Structure And Backend Development With Java/array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String[] args) {
+        int arr[][]={
+                {22,33,44,566,66},
+                {44,66,777,777}
+        };
+        int []hello={5,6,7,8,6};
+        System.out.println(arr);
+    }
+}
