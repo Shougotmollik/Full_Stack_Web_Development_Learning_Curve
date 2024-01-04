cfd44b7b18d2d5aa4863d0b8b40ffbea34ef1f2b
Shougot mollik <shougotmollik@gmail.com>
2024-01-04 20:50:45 +0600

    variable and data types

---
+++ Apna College(DeltaCourse)/JavaScript/Lecture 1/index.js
@@ -0,0 +1,14 @@
+let name = "shougot mollik";
+// console.log(name);
+
+const student = {
+  fullName: "shougot",
+  age: 20,
+  section: "Cse",
+};
+student.age = student.age + 1;
+console.log(student.age);
+
+function sum() {
+  return a + b;
+}
