ff4f825c5f1e1195309ecbd782573c4c684791b9
Shougot mollik <shougotmollik@gmail.com>
2024-01-17 22:30:33 +0600

    start React Native course

---
+++ React Native/awasome01/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import{
+  ScrollView,
+  View,
+  Text
+} from "react-native" ;
+
+function App(){
+  return(
+    <ScrollView>
+      <View style={{flex:1,backgroundColor:"plum"}}>
+        
+      </View>
+    </ScrollView>
+  );
+}
+
+export default App;
