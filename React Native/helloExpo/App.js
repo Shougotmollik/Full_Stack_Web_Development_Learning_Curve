70361129acae4cf3a2867097cc9ecec235b1d9a9
Shougot mollik <shougotmollik@gmail.com>
2024-01-19 01:16:15 +0600

    react native

--- React Native/helloExpo/App.js
+++ React Native/helloExpo/App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollViewComponent,
+  ScrollView,
+  Image,
+  ImageBackground,
+  Button,
+  Pressable,
+  Modal,
+  ActivityIndicator,
+} from "react-native";
+const imgload = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [isVisiable, setIsvisiable] = useState(false);
+
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={{ flex: 1 }}>
+      <StatusBar backgroundColor="limegreen" />
+      <ActivityIndicator />
     </View>
   );
 }
