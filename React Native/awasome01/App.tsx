752ee2af800a0c894adfe777d358b25889ab5a2c
Shougot mollik <shougotmollik@gmail.com>
2024-01-19 15:30:30 +0600

    React native

--- React Native/awasome01/App.tsx
+++ React Native/awasome01/App.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
-import {ScrollView, View, Text} from 'react-native';
-import AppPro from './AppPro';
+import {StyleSheet, Text, View} from 'react-native';
 
 function App() {
   return (
-    <ScrollView>
-      <View>
-        <Text>hello</Text>
-        <AppPro/>
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      <View style={[styles.box, styles.box1,styles.box_shadow]}></View>
+      <View style={[styles.box, styles.box2,styles.box_shadow]}></View>
+    </View>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:"aqua",
+    padding: 60,
+
+  },
+  box: {
+    height: 200,
+    width: 200,
+    margin: 20,
+    borderRadius: 25,
+    borderWidth:2,
+    
+  },
+  box1: {
+    backgroundColor: 'red',
+  },
+  box2: {
+    backgroundColor: 'yellow',
+  },
+  box_shadow:{
+    elevation:30,
+    shadowColor:"blue"
+
+  }
+});
