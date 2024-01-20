bee1c95ee5bbf775a4de12c02e5888d883a6aa4e
Shougot mollik <shougotmollik@gmail.com>
2024-01-21 00:41:34 +0600

    add everyThing

--- React Native/flatScrollApp/App.tsx
+++ React Native/flatScrollApp/App.tsx
@@ -1,36 +1,25 @@
-import React, { useState } from 'react'
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import FlatCards from './components/FlatCards'
+import ElevatedCard from './components/ElevatedCard'
 
-function App() {
-  const[counter,setCounter]=useState(0)
+const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.box}>
-        <Text>
-          count:{counter}
-        </Text>
-        <Button title='Increase'onPress={()=>{
-          setCounter(counter+1)
-        }} />
-        <Button title='Decrease' onPress={()=>{
-          setCounter(counter-1)
-        }}/>
-      </View>
+    <SafeAreaView style={styles.bodyColor}>
+      <ScrollView>
+        <FlatCards/>
+        <ElevatedCard/>
+      </ScrollView>
     </SafeAreaView>
   )
 }
 
-export default App;
+export default App
 
-const styles=StyleSheet.create({
-  container:{
+const styles = StyleSheet.create({
+  bodyColor:{
     flex:1,
-    backgroundColor:"lime"
-  },
-  box:{
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center"
-
+    backgroundColor:"grey",
+    
   }
-})
+});
