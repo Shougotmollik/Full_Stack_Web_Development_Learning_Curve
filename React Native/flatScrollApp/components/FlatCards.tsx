77d9d3e005c66f8711e676e826fac002b4d5c0d5
Shougot mollik <shougotmollik@gmail.com>
2024-01-21 00:41:20 +0600

    Flat card

---
+++ React Native/flatScrollApp/components/FlatCards.tsx
@@ -0,0 +1,54 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FlatCards = () => {
+  return (
+    <View>
+      <Text style={styles.headingText}>FlatCards</Text>
+      <View style={styles.container}>
+       <View style={[styles.card,styles.cardOne]}>
+        <Text>Red</Text>
+       </View>
+       <View style={[styles.card,styles.cardTwo]}>
+        <Text>Blue</Text>
+       </View>
+       <View style={[styles.card,styles.cardThree]}>
+        <Text>Green</Text>
+       </View>
+       
+      </View>
+    </View>
+  )
+}
+
+export default FlatCards
+
+const styles = StyleSheet.create({
+ headingText:{
+  fontSize:24,
+  fontWeight:"bold",
+  paddingHorizontal:10,
+ },
+ container:{
+  flex:1,
+  flexDirection:"row"
+ },
+ card:{
+  height:100,
+  width:100,
+  borderRadius:8,
+  margin:8,
+  flex:1,
+  justifyContent:"center",
+  alignItems:"center"
+ },
+ cardOne:{
+  backgroundColor:"red",
+ },
+ cardTwo:{
+  backgroundColor:"blue"
+ },
+ cardThree:{
+  backgroundColor:"green"
+ }
+})
