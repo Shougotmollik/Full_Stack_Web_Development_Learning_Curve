3fbe05cb3fed315003c0885cb0152bdfc8efb74b
Shougot mollik <shougotmollik@gmail.com>
2024-01-21 13:31:37 +0600

    implementation

--- React Native/flatScrollApp/App.tsx
+++ React Native/flatScrollApp/App.tsx
@@ -1,27 +1,28 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import FlatCards from './components/FlatCards'
-import ElevatedCard from './components/ElevatedCard'
-import FancyCard from './components/FancyCard'
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import FlatCards from './components/FlatCards';
+import ElevatedCard from './components/ElevatedCard';
+import FancyCard from './components/FancyCard';
+import ActionCard from './components/ActionCard';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.bodyColor}>
       <ScrollView>
-        <FlatCards/>
-        <ElevatedCard/>
-        <FancyCard/>
+        <FlatCards />
+        <ElevatedCard />
+        <FancyCard />
+        <ActionCard />
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
-  bodyColor:{
-    flex:1,
-    backgroundColor:"grey",
-    
-  }
+  bodyColor: {
+    flex: 1,
+    backgroundColor: '#333333',
+  },
 });
