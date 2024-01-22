ad27fa0140080d183df35c9e44d957eb92f6d713
Shougot mollik <shougotmollik@gmail.com>
2024-01-22 20:48:36 +0600

    Implementation

--- React Native/chatBox/App.tsx
+++ React Native/chatBox/App.tsx
@@ -1,10 +1,15 @@
 import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import StoriesList from './components/StoriesList';
+import ChatList from './components/ChatList';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView></ScrollView>
+      <ScrollView>
+        <StoriesList />
+        <ChatList />
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -13,7 +18,7 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#e2ddd9',
+    backgroundColor: '#00b4fc',
     flex: 1,
   },
 });
