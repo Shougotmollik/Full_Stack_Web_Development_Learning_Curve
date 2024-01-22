393d96b62a1ae017eadacfd3aa421819660f5b7d
Shougot mollik <shougotmollik@gmail.com>
2024-01-22 14:13:09 +0600

    Implementation

--- React Native/flatScrollApp/App.tsx
+++ React Native/flatScrollApp/App.tsx
@@ -4,6 +4,7 @@ import FlatCards from './components/FlatCards';
 import ElevatedCard from './components/ElevatedCard';
 import FancyCard from './components/FancyCard';
 import ActionCard from './components/ActionCard';
+import ContactList from './components/ContactList';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <ElevatedCard />
         <FancyCard />
         <ActionCard />
+        <ContactList />
       </ScrollView>
     </SafeAreaView>
   );
