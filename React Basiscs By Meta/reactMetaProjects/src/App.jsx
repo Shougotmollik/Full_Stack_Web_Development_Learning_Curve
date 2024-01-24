3dbca466b97351425d781e3dea680ea7543ffa4e
Shougot mollik <shougotmollik@gmail.com>
2024-01-24 11:37:09 +0600

    calculator app

--- React Basiscs By Meta/reactMetaProjects/src/App.jsx
+++ React Basiscs By Meta/reactMetaProjects/src/App.jsx
@@ -1,16 +1,57 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useRef } from "react";
 import "./App.css";
-import Heading from "./components/Heading";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  function plus(e) {
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value));
+  }
+
+  function minus(e) {
+    // Add the code for the minus function
+  }
+
+  function times(e) {
+    // Add the code for the plus function
+  }
+
+  function divide(e) {
+    // Add the code for the divide function
+  }
+
+  function resetInput(e) {
+    // Add the code for the resetInput function
+  }
+
+  function resetResult(e) {
+    // Add the code for the resetResult function
+  }
 
   return (
-    <>
-      <Heading />
-    </>
+    <div className="App">
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}>{/* add the value of the current total */}</p>
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+        <button onClick={plus}>add</button>
+        {/* Add the subtract button */}
+        {/* Add the multiply button */}
+        {/* Add the divide button */}
+        {/* Add the resetInput button */}
+        {/* Add the resetResult button */}
+      </form>
+    </div>
   );
 }
 
