5af0c1ce91345d2d50ba3ab0ce37ca0b130dfda1
Shougot mollik <shougotmollik@gmail.com>
2024-01-26 11:55:43 +0600

    finished the project

--- React Native/backgroundChangingApp/App.tsx
+++ React Native/backgroundChangingApp/App.tsx
@@ -1,24 +1,28 @@
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 
 const App = (): JSX.Element => {
+  const [randomBgColor, setRandomBgColor] = useState("#ffffff");
+  const generateColor = () => {
+    const hexRange = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)];
+    }
+    setRandomBgColor(color);
+  };
   return (
     <>
-      <StatusBar />
-      <SafeAreaView style={[styles.bgColor, styles.container]}>
-        <TouchableOpacity style={styles.actionBtn}>
-          <View>
+      <StatusBar backgroundColor={randomBgColor} />
+
+      <View style={[styles.container, { backgroundColor: randomBgColor }]}>
+        <TouchableOpacity onPress={generateColor}>
+          <View style={styles.actionBtn}>
             <Text style={styles.actionBtnTxt}> Press Here</Text>
           </View>
         </TouchableOpacity>
-      </SafeAreaView>
+      </View>
     </>
   );
 };
@@ -26,10 +30,6 @@ const App = (): JSX.Element => {
 export default App;
 
 const styles = StyleSheet.create({
-  bgColor: {
-    flex: 1,
-    backgroundColor: "#000000",
-  },
   container: {
     flex: 1,
     justifyContent: "center",
@@ -37,10 +37,13 @@ const styles = StyleSheet.create({
   },
   actionBtnTxt: {
     color: "white",
+    fontSize: 24,
+    textTransform: "uppercase",
   },
   actionBtn: {
     backgroundColor: "#0de5a8",
-    height: 50,
-    width: 150,
+    paddingVertical: 10,
+    borderRadius: 10,
+    paddingHorizontal: 20,
   },
 });
