0f0b82e70c78bdd4801b4cac92fc6487706ca3c6
Shougot mollik <shougotmollik@gmail.com>
2024-01-28 00:15:10 +0600

    HomeScreen

--- React Native/videoStreamingApp/App/Screen/HomeScreen.js
+++ React Native/videoStreamingApp/App/Screen/HomeScreen.js
@@ -1,11 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import Header from "../Components/Header";
+import Slider from "../Components/Slider";
+import PlayList from "../Components/PlayList";
+import VideoGridList from "../Components/VideoGridList";
+import BlogView from "../Components/BlogView";
 
 const HomeScreen = () => {
   return (
     <View>
       <Header />
+      <Slider />
+      <PlayList />
+      <VideoGridList />
+      <BlogView />
     </View>
   );
 };
