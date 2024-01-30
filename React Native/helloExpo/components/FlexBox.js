dc19d9c0d383911d56714caac606e695d3971f37
Shougot mollik <shougotmollik@gmail.com>
2024-01-30 21:14:02 +0600

    flex box

---
+++ React Native/helloExpo/components/FlexBox.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FlexBox = () => {
+  return (
+    <View>
+      <Text>FlexBox</Text>
+    </View>
+  )
+}
+
+export default FlexBox
+
+const styles = StyleSheet.create({
+ 
+})
