54f2209a15cf5fdc799cf1ac15fffe35041c2063
Shougot mollik <shougotmollik@gmail.com>
2024-01-30 21:14:29 +0600

    implementation

--- React Native/helloExpo/App.js
+++ React Native/helloExpo/App.js
@@ -1,40 +1,20 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import FlexBox from "./components/FlexBox";
 
-function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <View style={[styles.box, styles.box1, styles.box_shadow]}>
-        <Text>Hello</Text>
-      </View>
-      <View style={[styles.box, styles.box2, styles.box_shadow]}></View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>{/* <FlexBox /> */}</View>
+    </SafeAreaView>
   );
-}
+};
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "aqua",
-    padding: 60,
-  },
-  box: {
-    height: 200,
-    width: 200,
-    margin: 20,
-    borderRadius: 25,
-    borderWidth: 2,
-  },
-  box1: {
-    backgroundColor: "red",
-  },
-  box2: {
-    backgroundColor: "yellow",
-  },
-  box_shadow: {
-    elevation: 30,
-    shadowColor: "blue",
+    backgroundColor: "plum",
   },
 });
