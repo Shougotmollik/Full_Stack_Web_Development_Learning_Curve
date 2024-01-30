005cbad719760e59f34558dffa29002d38e57618
Shougot mollik <shougotmollik@gmail.com>
2024-01-30 21:14:10 +0600

    greet

--- React Native/helloExpo/components/Greet.js
+++ React Native/helloExpo/components/Greet.js
@@ -1,9 +1,42 @@
-import { View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Greet(name) {
+function App() {
   return (
-    <View>
-      <Text>Hello,{name}</Text>
+    <View style={styles.container}>
+      <View style={[styles.box, styles.box1, styles.box_shadow]}>
+        <Text style={{ color: "white" }}>Hello</Text>
+      </View>
+      <View style={[styles.box, styles.box2, styles.box_shadow]}></View>
     </View>
   );
 }
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "aqua",
+    padding: 60,
+  },
+  box: {
+    height: 200,
+    width: 200,
+    margin: 20,
+    borderRadius: 25,
+    borderWidth: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  box1: {
+    backgroundColor: "red",
+  },
+  box2: {
+    backgroundColor: "yellow",
+  },
+  box_shadow: {
+    elevation: 30,
+    shadowColor: "blue",
+  },
+});
